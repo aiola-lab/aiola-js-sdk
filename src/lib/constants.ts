400696e5d973504a90425131c05607b06c0de941
stavelmashally <stav.elmashally@aiola.com>
2025-12-11 15:56:26 +0200

    fix: delete aiola-js agent header

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,8 +3,6 @@ import { RUNTIME } from "./runtime";
 export const DEFAULT_BASE_URL = "https://apis.aiola.ai";
 export const DEFAULT_AUTH_BASE_URL = "https://auth.aiola.ai";
 
-export const DEFAULT_HEADERS = {
-  "User-Agent": `@aiola/aiola-js/${RUNTIME.type}/${RUNTIME.version}`,
-};
+export const DEFAULT_HEADERS = {};
 
 export const DEFAULT_WORKFLOW_ID = "2c78fcf1-9265-408f-b8c3-d9d7e7ebc1bc";
